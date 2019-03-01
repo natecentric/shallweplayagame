b25966e701b2a2615e8d0472392e8944b1878f07
natecentric <nate.davis@centricconsulting.com>
2019-03-01 11:44:03 -0800

    the first commit

---
+++ app.py
@@ -0,0 +1,233 @@
+import pandas as pd
+import openpyxl
+import urllib.request
+import json
+from pandas import DataFrame 
+from flask import Flask, request, redirect, g, render_template, session
+from spotify_requests import spotify
+from azureml_requests import azureml
+
+app = Flask(__name__)
+app.secret_key = 'some key for session'
+
+# ----------------------- AUTH API PROCEDURE -------------------------
+
+@app.route("/auth")
+def auth():
+    return redirect(spotify.AUTH_URL)
+
+
+@app.route("/callback/")
+def callback():
+
+    auth_token = request.args['code']
+    auth_header = spotify.authorize(auth_token)
+    session['auth_header'] = auth_header
+
+    return profile()
+
+def valid_token(resp):
+    return resp is not None and not 'error' in resp
+# -------------------------- API REQUESTS ----------------------------
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/search/')
+def search():
+    try:
+        search_type = request.args['search_type']
+        name = request.args['name']
+        return make_search(search_type, name)
+    except:
+        return render_template('search.html')
+
+
+@app.route('/search/<search_type>/<name>')
+def search_item(search_type, name):
+    return make_search(search_type, name)
+
+
+def make_search(search_type, name):
+    if search_type not in ['artist', 'album', 'playlist', 'track']:
+        return render_template('index.html')
+
+    data = spotify.search(search_type, name)
+    api_url = data[search_type + 's']['href']
+    items = data[search_type + 's']['items']
+
+    return render_template('search.html',
+                           name=name,
+                           results=items,
+                           api_url=api_url,
+                           search_type=search_type)
+
+
+@app.route('/artist/<id>')
+def artist(id):
+    artist = spotify.get_artist(id)
+
+    if artist['images']:
+        image_url = artist['images'][0]['url']
+    else:
+        image_url = 'http://bit.ly/2nXRRfX'
+
+    tracksdata = spotify.get_artist_top_tracks(id)
+    tracks = tracksdata['tracks']
+
+    related = spotify.get_related_artists(id)
+    related = related['artists']
+
+    return render_template('artist.html',
+                           artist=artist,
+                           related_artists=related,
+                           image_url=image_url,
+                           tracks=tracks)
+
+
+@app.route('/profile')
+def profile():
+    if 'auth_header' in session:
+        auth_header = session['auth_header']
+        # get profile data
+        profile_data = spotify.get_users_profile(auth_header)
+
+        # get user playlist data
+        playlist_data = spotify.get_users_playlists(auth_header)
+
+        # get user recently played tracks
+        recently_played = spotify.get_users_recently_played(auth_header)
+        
+        # get user top artist
+        top_artists_data = spotify.get_users_top_artists(auth_header)
+        
+        # get user top tracks
+        top_tracks_data = spotify.get_users_top_tracks(auth_header)
+        
+        if valid_token(recently_played):
+            return render_template("profile.html",
+                               user=profile_data,
+                               playlists=playlist_data["items"],
+                               recently_played=recently_played["items"],
+                               top_artists=top_artists_data["items"],
+                               top_tracks=top_tracks_data["items"])
+
+    return render_template('profile.html')
+
+
+@app.route('/playlist/<id>')
+def playlist(id):
+     if 'auth_header' in session:
+        auth_header = session['auth_header']
+        playlist_data = spotify.get_playlist(auth_header,id)
+        playlist_id = request.view_args['id']
+     return render_template('playlist.html', playlist=playlist_data["tracks"],playlist_id=playlist_id)
+
+@app.route('/featured_playlists')
+def featured_playlists():
+    if 'auth_header' in session:
+        auth_header = session['auth_header']
+        hot = spotify.get_featured_playlists(auth_header)
+        if valid_token(hot):
+            return render_template('featured_playlists.html', hot=hot)
+
+    return render_template('profile.html')
+
+@app.route('/track_list')
+def track_list():
+    track_list = spotify.get_several_tracks
+    tracks = track_list ['tracks']
+    return render_template ('track_list.html', track_list=track_list, tracks=tracks)
+
+@app.route('/playlist_audio_features')
+def playlist_audio_features():
+    if 'auth_header' in session:
+        auth_header = session['auth_header']
+        offset = 0
+        songs = []
+        items = []
+        ids = []
+        track_info = []
+        id = request.args['id']
+        # get playlist
+        while True:
+            content = spotify.get_playlist_tracks(auth_header, id)
+            songs += content['items']
+            if content['next'] is not None:
+                offset += 100
+            else:
+                break
+
+        for i in songs:
+            ids.append(i['track']['id'])
+            track_info.append([
+                i['track']['id'],
+                i['track']['name'],
+                i['track']['popularity']
+                ])
+
+        trackdf = pd.DataFrame(track_info, columns=
+                          [
+                          'id',
+                          'name',
+                          'popularity'
+                          ]
+                           )
+        # get playlist features
+        index = 0
+        global playlist_audio_features
+        #playlist_audio_features = spotify.get_audio_feature(auth_header, ids)
+        while index < len(ids):
+            playlist_audio_features = spotify.get_audio_feature(auth_header,ids[index:index + 50])
+            index += 50
+        # build playlist features
+        features_list = []
+        for features in playlist_audio_features['audio_features']:
+            features_list.append([
+                features['id'],
+                features['acousticness'],features['danceability'],
+                features['energy'],features['liveness'],
+                features['speechiness'],features['valence']
+                ])
+
+
+
+        featuredf = pd.DataFrame(features_list, columns=
+                          [
+                          'id',  
+                          'acousticness', 'danceability',
+                          'energy', 'liveness',
+                          'speechiness', 'valence'
+                          ]
+                           )
+        #alldf = pd.concat([trackdf, featuredf], axis=1, join='inner')
+        alldf = pd.merge(trackdf, featuredf, on='id')
+        mlinputdf = pd.DataFrame(features_list, columns=
+                    [
+                    'id',  
+                    'acousticness', 'danceability',
+                    'energy', 'liveness',
+                    'speechiness', 'valence'
+                    ]
+                    )
+        mlinputdf.to_csv('mlinput.csv', sep = ',', index = False)
+        return render_template('playlist_audio_features.html',  featuredata=featuredf.to_html(), trackdata=trackdf.to_html(), alldata=alldf.to_html())
+
+
+@app.route('/mloutput')
+def mloutput():
+    mlbatchrequest = azureml.invokeBatchExecutionService()
+    mloutputtable = pd.read_csv('mloutput.csv')
+    return render_template("mloutput.html", data=mloutputtable.to_html())
+
+
+
+
+
+if __name__ == "__main__":
+    
+    app.run(debug=True, port=spotify.PORT)
+
---
+++ azureml_requests/azureml.py
@@ -0,0 +1,159 @@
+# How this works:
+#
+# 1. Assume the input is present in a local file (if the web service accepts input)
+# 2. Upload the file to an Azure blob - you"d need an Azure storage account
+# 3. Call BES to process the data in the blob.
+# 4. The results get written to another Azure blob.
+# 5. Download the output blob to a local file
+#
+# Note: You may need to download/install the Azure SDK for Python.
+# See: http://azure.microsoft.com/en-us/documentation/articles/python-how-to-install/
+
+import urllib.request
+import json
+import time
+from azure.storage.blob import *
+
+def printHttpError(httpError):
+    print("The request failed with status code: " + str(httpError.code))
+
+    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+    print(httpError.info())
+
+    print(json.loads(httpError.read().decode("utf8", 'ignore')))
+    return
+
+def saveBlobToFile(blobUrl, resultsLabel):
+    output_file = "mloutput.csv" # Replace this with the location you would like to use for your output file, and valid file extension (usually .csv for scoring results, or .ilearner for trained models)
+    print("Reading the result from " + blobUrl)
+    try:
+        response = urllib.request.urlopen(blobUrl)
+    except urllib.error.HTTPError as error:
+        printHttpError(error)
+        return
+
+    with open(output_file, "w+") as f:
+        f.write(response.read().decode("utf8", 'ignore'))
+    print(resultsLabel + " have been written to the file " + output_file)
+    return
+
+def processResults(result):
+    first = True
+    results = result["Results"]
+    for outputName in results:
+        result_blob_location = results[outputName]
+        sas_token = result_blob_location["SasBlobToken"]
+        base_url = result_blob_location["BaseLocation"]
+        relative_url = result_blob_location["RelativeLocation"]
+
+        print("The results for " + outputName + " are available at the following Azure Storage location:")
+        print("BaseLocation: " + base_url)
+        print("RelativeLocation: " + relative_url)
+        print("SasBlobToken: " + sas_token)
+
+        if (first):
+            first = False
+            url3 = base_url + relative_url + sas_token
+            saveBlobToFile(url3, "The results for " + outputName)
+    return
+
+def uploadFileToBlob(input_file, input_blob_name, storage_container_name, storage_account_name, storage_account_key):
+    blob_service = BlockBlobService(account_name=storage_account_name, account_key=storage_account_key)
+
+    print("Uploading the input to blob storage...")
+    blob_service.create_blob_from_path(storage_container_name, input_blob_name, input_file)
+
+def invokeBatchExecutionService():
+    storage_account_name = "z" # Replace this with your Azure Storage Account name
+    storage_account_key = "y" # Replace this with your Azure Storage Key
+    storage_container_name = "x" # Replace this with your Azure Storage Container name
+    connection_string = "DefaultEndpointsProtocol=https;AccountName=" + storage_account_name + ";AccountKey=" + storage_account_key
+
+    api_key = "w" # Replace this with the API key for the web service
+    url = "https://ussouthcentral.services.azureml.net/subscriptions/76a0821cb1ea416bac58dc752dc03eaa/services/292ac9fa0a624350b38071e8421d8a8d/jobs"
+
+    uploadFileToBlob("mlinput.csv", # Replace this with the location of your input file, and valid file extension (usually .csv)
+        "mlinputblob.csv", # Replace this with the name you would like to use for your Azure blob; this needs to have the same extension as the input file 
+        storage_container_name, storage_account_name, storage_account_key);
+
+    payload = {
+            "Inputs": {
+                    "input1": 
+                    {
+                        "ConnectionString": connection_string,
+                        "RelativeLocation": "/" + storage_container_name + "/mlinputblob.csv"
+                    },
+            },
+
+            "Outputs": {
+                    "output1":
+                    {
+                        "ConnectionString": connection_string,
+                        "RelativeLocation": "/" + storage_container_name + "/mloutputblob.csv" # Replace this with the location you would like to use for your output file, and valid file extension (usually .csv for scoring results, or .ilearner for trained models)
+                    },
+            },
+
+        "GlobalParameters": {
+        }
+    }
+
+    body = str.encode(json.dumps(payload))
+    headers = { "Content-Type":"application/json", "Authorization":("Bearer " + api_key)}
+    print("Submitting the job...")
+
+    # submit the job
+    req = urllib.request.Request(url + "?api-version=2.0", body, headers)
+
+    try:
+        response = urllib.request.urlopen(req)
+    except urllib.error.HTTPError as error:
+        printHttpError(error)
+        return
+
+    result = response.read()
+    job_id = result.decode("utf8", 'ignore')[1:-1]
+    print("Job ID: " + job_id)
+
+    # start the job
+    print("Starting the job...")
+    body = str.encode(json.dumps({}))
+    req = urllib.request.Request(url + "/" + job_id + "/start?api-version=2.0", body, headers)
+    try:
+        response = urllib.request.urlopen(req)
+    except urllib.error.HTTPError as error:
+        printHttpError(error)
+        return
+
+    url2 = url + "/" + job_id + "?api-version=2.0"
+
+    while True:
+        print("Checking the job status...")
+        req = urllib.request.Request(url2, headers = { "Authorization":("Bearer " + api_key) })
+
+        try:
+            response = urllib.request.urlopen(req)
+        except urllib.error.HTTPError as error:
+            printHttpError(error)
+            return
+
+        result = json.loads(response.read().decode("utf8", 'ignore'))
+        status = result["StatusCode"]
+        if (status == 0 or status == "NotStarted"):
+            print("Job " + job_id + " not yet started...")
+        elif (status == 1 or status == "Running"):
+            print("Job " + job_id + " running...")
+        elif (status == 2 or status == "Failed"):
+            print("Job " + job_id + " failed!")
+            print("Error details: " + result["Details"])
+            break
+        elif (status == 3 or status == "Cancelled"):
+            print("Job " + job_id + " cancelled!")
+            break
+        elif (status == 4 or status == "Finished"):
+            print("Job " + job_id + " finished!")
+            processResults(result)
+            break
+        time.sleep(1) # wait one second
+    return
+
+
